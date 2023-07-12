0af8b3979b9029ca974666144335548250021026
Senyo Motey <moteysenyo@gmail.com>
2023-07-12 10:47:25 +0000

    created a file to contain color definitions

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = const Color(0xFF252525);
+
+Color actionBarItemBackgroundColor = const Color(0xFF3B3B3B);
+
+Color actionBarItemIconColor = Colors.white;
