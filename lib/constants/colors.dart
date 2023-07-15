da64d99e80b26fb0e534c2249c78e0cd60c6c6f8
Senyo Motey <moteysenyo@gmail.com>
2023-07-15 06:23:40 +0000

    Made UI improvements

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -16,10 +16,10 @@ Color negativeButtonColor = Colors.redAccent; // const Color(0xFFFF0000);
 
 Color errorSnackbarColor = const Color.fromARGB(255, 174, 1, 1);
 
-Color redNoteColor = Colors.redAccent;
+Color redNoteColor = Color.fromARGB(255, 248, 137, 137);
 Color orangeNoteColor = Colors.orangeAccent;
-Color yellowNoteColor = Colors.yellowAccent;
-Color greenNoteColor = Colors.greenAccent;
-Color blueNoteColor = Colors.blueAccent;
-Color indigoNoteColor = Colors.indigoAccent;
+Color yellowNoteColor = Color.fromARGB(255, 248, 248, 99);
+Color greenNoteColor = Color.fromARGB(255, 147, 245, 198);
+Color blueNoteColor = Color.fromARGB(255, 120, 169, 253);
+Color indigoNoteColor = Color.fromARGB(255, 135, 152, 245);
 Color whiteNoteColor = Colors.white;
